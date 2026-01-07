b99c41bb45dee0b523461a2cdc4e50eeb8f9f858
ramsatendra <ram.satendra@gmail.com>
2026-01-07 13:11:00 +0530

    new files

---
+++ loop2.py
@@ -0,0 +1,6 @@
+import time
+x = 2
+while True:
+    print("value of x is ", x)
+    time.sleep(1)
+    x = x + 1
---
+++ loops.py
@@ -0,0 +1,17 @@
+#for loop
+Planet = "Earth"
+for i in Planet:
+    print(i)
+
+# using tuple
+Vaccines = ("Astra", "sputnik", "covaxin", "pfizer")
+for vack in Vaccines:
+    print(f"{vack} helps in immunization")
+
+# while loop
+x = 0
+while x < 10:
+    print("the value of x is ", x)
+    print("looping")
+    x = x + 1
+    print("rest of the code")
